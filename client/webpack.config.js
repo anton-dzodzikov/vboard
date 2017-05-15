83204eefb451cbdbd619f66f91c1aea38f9d9ed3
Anton Dzodzikov <anton.dzodzikov@gmail.com>
2017-05-15 21:31:23 +1000

    Savepoint

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,8 @@
 var path = require('path')
 var webpack = require('webpack')
+var NpmInstallPlugin = require('npm-install-webpack-plugin')
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
@@ -14,9 +17,9 @@ module.exports = {
         publicPath: '/static/'
     },
     plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new NpmInstallPlugin()
     ],
     module: {
         preLoaders: [
@@ -36,7 +39,14 @@ module.exports = {
                 ],
                 test: /\.js$/,
                 plugins: ['transform-runtime'],
+            },
+            {
+                test:   /\.css$/,
+                loader: "style-loader!css-loader!postcss-loader"
             }
         ]
+    },
+    postcss: function () {
+        return [autoprefixer, precss];
     }
 }
